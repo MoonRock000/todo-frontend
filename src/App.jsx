4ee7a65a2c98252733475093ac5b91fc6a5bd923
MoonRock000 <morrisemin@gmail.com>
2024-06-06 00:00:39 +0500

    Created basic grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Grid
+      templateAreas={`"header"
+                      "main"
+                      "footer"`}
+      gap="1"
+      color="blackAlpha.700"
+    >
+      <GridItem pl="2" bg="orange.300" area={"header"}>
+        Header
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 }
 
