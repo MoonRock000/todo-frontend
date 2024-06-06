b0fb14614008d8811cb8d33043560b52daec4a5d
MoonRock000 <morrisemin@gmail.com>
2024-06-07 04:10:27 +0500

    Moved base url to api config

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { getToken } from "../storage/sessionStorage";
+import { config } from "../config/apiConfig";
 
-export const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
-});
+export const apiClient = axios.create(config);
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    try {
+      const token = getToken();
+      if (token) {
+        console.log("token foundd");
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error("Error retrieving token", error);
+      // Optionally, handle the error if needed
     }
     return config;
   },
